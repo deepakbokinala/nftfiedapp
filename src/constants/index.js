838e80d082884ec6f3f0d5da69dbd5af7a79afa6
deepakbokinala <59043111+deepakbokinala@users.noreply.github.com>
2022-11-15 13:05:25 +0530

    version 1.0

---
+++ src/constants/index.js
@@ -0,0 +1,193 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, qb, thub, polygon, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Collection Generator",
+    content:
+      "We provide the fastest NFT generator in the market. You can generate 100 unique images for free. We charge with a fixed price of $25 per generation for collections above 100.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Minting Website Hosting",
+    content:
+      "Create a minting website by a click of a button. You can host your own minting website for free. Features including prebuilt templates, addons, domain, and more. Unlock special features by upgrading to premium for $15 USD.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Metadata Utilities",
+    content:
+      "Create a minting website by a click of a button. You can host your own minting website for free. Features including prebuilt templates, addons, domain, and more. Unlock special features by upgrading to premium for $15 USD.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Bharath Silagani",
+    title: "Community Manager, Chainlink India",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "User Active",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Trusted by Company",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "#",
+      },
+      {
+        name: "How it Works",
+        link: "#",
+      },
+      {
+        name: "Create",
+        link: "#",
+      },
+      {
+        name: "Explore",
+        link: "#",
+      },
+      {
+        name: "Terms & Services",
+        link: "#",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: qb,
+  },
+  {
+    id: "client-2",
+    logo: polygon,
+  },
+  {
+    id: "client-3",
+    logo: thub,
+  },
+];
